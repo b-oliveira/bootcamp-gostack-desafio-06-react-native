91a6409166b40f2724d4c98610727a2237fe0267
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-08-04 15:15:31 -0300

    Add página principal

---
+++ src/pages/Main/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    user: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { user, users } = this.state;
+
+    const response = await api.get(`/users/${user}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      user: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { user, users } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={user}
+            onChangeText={text => this.setState({ user: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={16} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 32px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 16px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 16px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 8px;
+  padding: 0 16px;
+`;
